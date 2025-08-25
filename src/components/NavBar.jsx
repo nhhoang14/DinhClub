213386b079c94c4cd7d35e5fa3efe833880ac829
nhhoang14 <nhhoangne@gmail.com>
2025-08-25 18:37:45 +0700

    update narbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import Cart from '../components/Cart';
 function NavBar() {
   const location = useLocation();
 
-   const handleTopClick = () => {
+  const handleTopClick = () => {
     if (location.pathname === location.pathname) {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     }
@@ -20,11 +20,17 @@ function NavBar() {
         </Link>
       </div>
       <ul className="nav-links">
-        <li><Link to="/" onClick={handleTopClick}>Home Page</Link></li>
+        <li><Link to="/" className= "nav-link" onClick={handleTopClick}>
+          Home Page
+        </Link></li>
         <li><p className='divide'>x</p></li>
-        <li><Link to="/products" onClick={handleTopClick}>Products</Link></li>
+        <li><Link to="/products" className= "nav-link" onClick={handleTopClick}>
+          Products
+        </Link></li>
         <li><p className='divide'>x</p></li>
-        <li><Link to="/contact" onClick={handleTopClick}>Contact Us</Link></li>
+        <li><Link to="/contact" className= "nav-link" onClick={handleTopClick}>
+          Contact Us
+        </Link></li>
       </ul>
       <div className="nav-cart">
         <Cart />
