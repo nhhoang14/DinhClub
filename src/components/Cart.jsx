48934a8919266b8f2d4ba46ece0e19b1c1be26d0
nhhoang14 <nhhoangne@gmail.com>
2025-08-27 14:59:37 +0700

    update cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,15 +1,66 @@
-import '../css/Cart.css';
+import '../css/Cart.css'
+import { useState } from 'react';
+import CartItem from './CartItem.jsx';
+import bedao from '../images/bedao.jpg';
+import bemai from '../images/bemai.jpg';
+import bequat from '../images/bequat.jpg';
+import bety from '../images/bety.jpg';
+import summer_st from '../images/summer_st.jpg';
+import xmas_st from '../images/xmas_st.jpg';
+import ninhbo_st from '../images/ninhbo_st.jpg';
 
 function Cart() {
-    return (
-        <div className="cart-container">
-            <button className="cart-btn">
-                <span className="material-symbols-outlined cart-icon">
-                    shopping_cart
-                </span>
-            </button>
+  const [showCart, setShowCart] = useState(false);
+
+  const cartItems = [
+    { image: bedao, title: "BÉ ĐÀO", price: 150000, qty: 2, stock: 10 },
+    { image: bemai, title: "BÉ MAI", price: 150000, qty: 1, stock: 5 },
+    { image: bequat, title: "BÉ QUẤT", price: 150000, qty: 3, stock: 8 },
+    { image: bety, title: "BÉ TỴ", price: 150000, qty: 2, stock: 6 },
+    { image: summer_st, title: "HÈ TUYỆT ĐỐI", price: 39000, qty: 5, stock: 12 },
+    { image: xmas_st, title: "GIÁNG SINH AN GIẤC", price: 39000, qty: 4, stock: 9 },
+    { image: ninhbo_st, title: "DÍNH NỊNH BỢ", price: 59000, qty: 2, stock: 7 },
+  ];
+
+  return (
+    <div className="cart-wrapper">
+      <button className="cart-btn" onClick={() => setShowCart(!showCart)}>
+        <span className="material-symbols-outlined cart-icon">
+          shopping_cart
+        </span>
+      </button>
+
+      <div className={showCart ? "cart-dropdown open" : "cart-dropdown"}>
+        <h4>GIỎ HÀNG ({cartItems.length})</h4>
+
+        <div className="cart-items-list">
+          {cartItems.length === 0 ? (
+            <p>Chưa có sản phẩm nào</p>
+          ) : (
+            <ul>
+              {cartItems.map((item, idx) => (
+                <CartItem key={idx} item={item} />
+              ))}
+            </ul>
+          )}
         </div>
-    );
+
+        <div className="cart-dropdown-footer">
+          <div className="price-detail">
+            <p><strong>Tổng tiền:</strong></p>
+            <p className="total-price">
+              {cartItems
+                .reduce((total, item) => total + item.price * item.qty, 0)
+                .toLocaleString('vi-VN')
+              } VND
+            </p>
+          </div>
+          <button className="checkout-btn">THANH TOÁN</button>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,36 +1,16 @@
 import '../css/NavBar.css';
 import logo from '../images/logo.png';
-import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import CartDropdown from '../components/CartDropdown.jsx';
-import bedao from '../images/bedao.jpg';
-import bemai from '../images/bemai.jpg';
-import bequat from '../images/bequat.jpg';
-import bety from '../images/bety.jpg';
-import summer_st from '../images/summer_st.jpg';
-import xmas_st from '../images/xmas_st.jpg';
-import ninhbo_st from '../images/ninhbo_st.jpg';
+import Cart from '../components/Cart.jsx';
 
 function NavBar() {
   const location = useLocation();
-
-  const [showCart, setShowCart] = useState(false);
 
   const handleTopClick = () => {
     if (location.pathname === location.pathname) {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     }
   };
-
-  const cartItems = [
-    { image: bedao, title: "BÉ ĐÀO", price: 150000, qty: 2, stock: 10},
-    { image: bemai, title: "BÉ MAI", price: 150000, qty: 1, stock: 5 },
-    { image: bequat, title: "BÉ QUẤT", price: 150000, qty: 3, stock: 8 },
-    { image: bety, title: "BÉ TỴ", price: 150000, qty: 2, stock: 6 },
-    { image: summer_st, title: "HÈ TUYỆT ĐỐI", price: 39000, qty: 5, stock: 12 },
-    { image: xmas_st, title: "GIÁNG SINH AN GIẤC", price: 39000, qty: 4, stock: 9 },
-    { image: ninhbo_st, title: "DÍNH NỊNH BỢ", price: 59000, qty: 2, stock: 7 },
-  ];
 
   return (
     <nav className="navbar">
@@ -54,12 +34,7 @@ function NavBar() {
       </ul>
       {/* cart */}
       <div className="nav-cart">
-        <button className="cart-btn" onClick={() => setShowCart(!showCart)}>
-          <span className="material-symbols-outlined cart-icon">
-            shopping_cart
-          </span>
-        </button>
-        {showCart && <CartDropdown cartItems={cartItems} />}
+       <Cart />
       </div>
     </nav>
   );
